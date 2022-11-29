06f37f921a6a239cdf5ad13f956734990481ec3d
kuninaa19 <minchan0527@naver.com>
2022-11-29 12:43:27 +0900

    feat(randomBallNumber): 랜덤 볼 생성 기능 추가

---
+++ src/main/java/baseball/RandomBallsGenerator.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import baseball.constants.BallConstants;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomBallsGenerator {
+    private static final Random random = ThreadLocalRandom.current();
+
+    public static Balls createRandomBalls() {
+        Set<Integer> randomNumbers = new LinkedHashSet<>();
+
+        while (randomNumbers.size() < BallConstants.COUNT) {
+            int randomNumber = randomNumberInRange();
+            randomNumbers.add(randomNumber);
+        }
+
+        return new Balls(new ArrayList<>(randomNumbers));
+    }
+
+    private static int randomNumberInRange() {
+        return BallConstants.MIN + random.nextInt(BallConstants.MAX - BallConstants.MIN + 1);
+    }
+}
---
+++ src/test/java/baseball/RandomNumberGeneratorTest.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomNumberGeneratorTest {
+    @Test
+    void 중복되지않는_랜덤_볼_생성() {
+        Balls balls = RandomBallsGenerator.createRandomBalls();
+        assertThat(balls).isNotNull();
+    }
+}
